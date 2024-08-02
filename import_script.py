cd286cbee3753c22afdb1602ebea988bcf889b00
Keenanandrew <63597895+keenanandrew@users.noreply.github.com>
2024-08-02 16:16:29 +0100

    first step

--- import_script.py
+++ import_script.py
@@ -1,27 +1,21 @@
 import csv
 import json
 
-def csv_to_nested_json(csv_file_path):
+def csv_to_json(csv_file_path):
+    data = []
     with open(csv_file_path, mode='r', newline='', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
-        data = {}
         for row in reader:
-            current_level = data
-            for key in row:
-                if row[key] != '':
-                    if key not in current_level:
-                        current_level[key] = {}
-                    current_level = current_level[key]
-            current_level.update(row)
+            data.append(row)
     return data
 
 # Replace 'your_file.csv' with the path to your CSV file
-csv_file_path = 'your_file.csv'
-nested_json = csv_to_nested_json(csv_file_path)
+csv_file_path = 'test_info1.csv'
+json_data = csv_to_json(csv_file_path)
 
 # Pretty-print the JSON data
-print(json.dumps(nested_json, indent=4))
+print(json.dumps(json_data, indent=4))
 
 # Optionally, write the JSON data to a file
 with open('output.json', 'w', encoding='utf-8') as jsonfile:
-    json.dump(nested_json, jsonfile, indent=4)
+    json.dump(json_data, jsonfile, indent=4)
