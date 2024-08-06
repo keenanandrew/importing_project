e2faed9df88197a39780683a5abd572824466140
Keenanandrew <63597895+keenanandrew@users.noreply.github.com>
2024-08-06 18:37:59 +0100

    updated

--- import_script.py
+++ import_script.py
@@ -1,30 +1,57 @@
 import csv
 import json
-from collections import defaultdict
+import pandas as pd
 
-def csv_to_json(csv_file_path):
-    data = [] # create an empty list
-    with open(csv_file_path, mode='r', newline='', encoding='utf-8') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            data.append(row)
-    return data # now it's a list of dictionaries
 
 def csv_to_nested_json(csv_file_path):
-    root = defaultdict(list)  # Using defaultdict to automatically create lists for new keys
-    with open(csv_file_path, mode='r', newline='', encoding='utf-8') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            conference_name = row.pop('name')  # Remove the 'name' key from the row and get its value
-            root[conference_name].append(row)  # Add the row (talk details) under the appropriate conference
+    df = pd.read_csv(csv_file_path)
 
-    return root
+    all_series = {}
+
+    organisers = [
+        {"user_id": 1, "credited_organiser": False},
+        {"user_id": 12, "credited_organiser": False},
+        {"user_id": 13, "credited_organiser": False},
+    ]
+
+    for index, row in df.iterrows():
+        if row["series_name"] not in all_series:
+            all_series[row["series_name"]] = { 'description': None, 'events': [] }
+
+        series = all_series[row["series_name"]]
+
+        series['name'] = row['series_name']
+        series['description'] = row["description"]
+
+        series['events'].append({
+            "start": row["event_start"],
+            "end": row["event_end"],
+            "timezone": row["event_timezone"],
+            "status": row["event_status"],
+            "talks": [
+                {
+                    'title': row["talk_title"],
+                    'abstract': row["talk_abstract"],
+                    'speakers': [{
+                        "name": row["speaker_name"],
+                        "email": row["speaker_email"],
+                    }],
+                }
+            ],
+            "organisers": organisers,
+        })
+
+    output = {
+        'event_series': list(all_series.values())
+    }
+
+    return output
 
 
 
 # Replace 'your_file.csv' with the path to your CSV file
-csv_file_path = 'test_info1.csv'
-json_data = csv_to_nested_json(csv_file_path) # the function returns a list of dictionaries
+csv_file_path = 'file_for_import1.csv'
+json_data = csv_to_nested_json(csv_file_path) # 
 
 # Pretty-print the JSON data
 print(json.dumps(json_data, indent=4))
